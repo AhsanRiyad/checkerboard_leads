03179105cfeaa5c5e692e6d96a2effd9c8a9c2fe
AhsanRiyad <riyad298@gmail.com>
2021-01-25 09:19:09 +0600

    checker board > design completed

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
+ 
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <div class="flexContainer" v-for="i in 8" :key="i">
+    <div v-for="j in 8" :key="j">
+    <div 
+    :style="{backgroundColor: ((i+j)%2) == 0 ? teamABoxColor : teamBBoxColor }"
+    v-bind:class="[ 'box'  ]"></div>
+    </div>
+    </div>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  name: 'CheckerBoard',
+  data(){
+    return {
+    teamABoxColor: "#ECF0F1",
+    teamBBoxColor: '#374360',
+    }
+  }
 };
 </script>
+
+<style>
+.box{
+  height: 100px;
+  width: 100px;
+  border: 1px solid white;
+}
+.blackBox{
+  background-color: black;
+}
+.greyBox{
+  background-color: grey;
+}
+.flexContainer{
+  display: flex;
+  width: 80%;
+  margin: 0 auto;
+  justify-content: center;
+}
+</style>
